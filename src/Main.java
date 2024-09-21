3f2cf9175cfa5f673cf243bd7cc0b474e35e975b
Hartlern <hartlern@mail.uc.edu>
2024-09-20 22:14:47 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        double doubleSummer = 500;
+        double doubleFall = 5660.05;
+        double doubleWinter = 560.2;
+        double doubleSpring = 600;
+        double doubleCostSeasons = doubleSummer + doubleFall + doubleWinter + doubleSpring;
+
+        System.out.println("The maintainence costs are [Summer, fall, winter, spring]: " + doubleSummer + ", " + doubleFall + ", " + doubleWinter + ", " + doubleSpring + ". The sum of the maintainence costs for this year is: " + doubleCostSeasons);
+
+
+    }
+}
